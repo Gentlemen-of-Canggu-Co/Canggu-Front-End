a0f220109f5e76caf3e26efcf39af1c7d6010527
Jan Auer <janauer2@gmx.de>
2023-01-28 01:24:32 +0800

    moved SpotDetailsPage into components, render events and consumables on SpotDetailsPage

---
+++ src/components/SpotDetailsPage/Menu.jsx
@@ -0,0 +1,15 @@
+function Menu(props){
+
+
+    const {spot} = props
+
+    return(
+
+<div>
+    <img style={{width: "100vw"}} src={spot.menuImage} alt="pictogram" />
+</div>
+
+    )
+}
+
+export default Menu
---
+++ src/components/SpotDetailsPage/Pictograms.jsx
@@ -0,0 +1,17 @@
+function Pictograms(props){
+    const {spot} = props
+
+    return(
+<div>
+<img style={{width: "50px"}} src="https://chemicalsinourlife.echa.europa.eu/documents/2934490/2940794/PICTOGRAM.png" alt="pictogram" />
+<img style={{width: "50px"}} src="https://chemicalsinourlife.echa.europa.eu/documents/2934490/2940794/PICTOGRAM.png" alt="pictogram" />
+<img style={{width: "50px"}} src="https://chemicalsinourlife.echa.europa.eu/documents/2934490/2940794/PICTOGRAM.png" alt="pictogram" />
+<img style={{width: "50px"}} src="https://chemicalsinourlife.echa.europa.eu/documents/2934490/2940794/PICTOGRAM.png" alt="pictogram" />
+<img style={{width: "50px"}} src="https://chemicalsinourlife.echa.europa.eu/documents/2934490/2940794/PICTOGRAM.png" alt="pictogram" />
+<img style={{width: "50px"}} src="https://chemicalsinourlife.echa.europa.eu/documents/2934490/2940794/PICTOGRAM.png" alt="pictogram" />
+<img style={{width: "50px"}} src="https://chemicalsinourlife.echa.europa.eu/documents/2934490/2940794/PICTOGRAM.png" alt="pictogram" />
+</div>
+    )
+}
+
+export default Pictograms
