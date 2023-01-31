88f660c36b899a644ff232d509e8b0d6477e7be6
Jan Auer <janauer2@gmx.de>
2023-01-31 21:23:59 +0800

    correct API URL endpoint for AddConsumable component

--- src/components/SpotDetailsPage/AddConsumable.jsx
+++ src/components/SpotDetailsPage/AddConsumable.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 function AddConsumable(props) {
   const { ownerId } = props;
-  const API_URL = "http://localhost:5005";
+  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
   const [consumable, setConsumable] = useState({
     name: "",
     tagline: "",
