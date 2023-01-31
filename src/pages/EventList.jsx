db957ea87aa36ece7cffff20f58ec18122e542f5
John <jayblez@gmail.com>
2023-01-31 23:05:57 +0100

    ...

--- src/pages/EventList.jsx
+++ src/pages/EventList.jsx
@@ -9,7 +9,7 @@ function EventList() {
   const [spots, setSpots] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
   const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5005";
-
+  
   useEffect(() => {
     setIsLoading(true);
     axios
