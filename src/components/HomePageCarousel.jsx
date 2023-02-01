048ecd0b3b252b8c7be751f89424b3c20cc915d3
John <jayblez@gmail.com>
2023-02-02 00:43:24 +0100

    Naaaaailed the homepage, lemme know what ya think

--- src/components/HomePageCarousel.jsx
+++ src/components/HomePageCarousel.jsx
@@ -1,46 +1,39 @@
-import Carousel from 'react-bootstrap/Carousel';
 import Bali1 from '../assets/images/Bali1.jpg';
 import Bali2 from '../assets/images/Bali2.jpg';
 import Bali3 from '../assets/images/Bali3.jpg';
+
+
 function HomePageCarousel() {
   return (
-    <Carousel className="carousel" fade>
-      <Carousel.Item interval={5000}>
-        <img
-          className="d-block w-100"
-          src={Bali1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>We don't offer a lot</h3>
-          <p>But what we do offer...</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={5000}>
-        <img
-          className="d-block w-100"
-          src={Bali2}
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Is a guided tour of Canggu</h3>
-          <p>And a chance to discover it's best bars, restaurants, and cafes...</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={5000}>
-        <img
-          className="d-block w-100"
-          src={Bali3}
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>So join us at our table, by the pool, or at the bar</h3>
-          <p>
-            And let's chow down in Canggu!
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <div>
+    <div id="carouselExampleIndicators" class="carousel slide my-carousel my-carousel" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active" style={{backgroundImage: `url(${Bali1})`}}>
+
+        </div>
+          <div class="carousel-item " style={{backgroundImage: `url(${Bali2})`}}>
+
+        </div>
+        <div class="carousel-item " style={{backgroundImage: `url(${Bali3})`}}>
+
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+    </div>
   );
 }
+
 export default HomePageCarousel;
