03f9df0ab0e6faa243be3dd4442ccb9acb9b1541
Jan Auer <janauer2@gmx.de>
2023-03-22 11:47:39 +0800

    change copy on spotlist

--- src/pages/SpotList.jsx
+++ src/pages/SpotList.jsx
@@ -54,7 +54,7 @@ if(isLoading){
     <div>
       {/* <Breadcrumbs spots={spots} /> */}
 
-      <h1 className="spotlist-headline">Spots in Canggu</h1>
+      <h1 className="spotlist-headline">Cafes & Restaurants in Canggu</h1>
       <p className="spotlist-p">Find your perfect spot, based on what matters to you.</p>
       <div>
         <SliderFilter spots={spots} setFilteredSpots={setFilteredSpots} />
@@ -65,17 +65,9 @@ if(isLoading){
                     <button type="button" className="btn btn-success" style={{margin: '10px'}}>Add new Spot</button>
                     </Link>   
             )}
-      <h2>SEO: About Cafes and Restaurants in Canggu</h2>
+      <h2>What makes the Cafes and Restaurants in Canggu special?</h2>
       <p>
-        A coffeehouse, coffee shop, or café is an establishment that primarily
-        serves coffee of various types, notably espresso, latte, and cappuccino.
-        Some coffeehouses may serve cold drinks, such as iced coffee and iced
-        tea, as well as other non-caffeinated beverages. In continental Europe,
-        cafés serve alcoholic drinks. A coffeehouse may also serve food, such as
-        light snacks, sandwiches, muffins, fruit, or pastries. Coffeehouses
-        range from owner-operated small businesses to large multinational
-        corporations. Some coffeehouse chains operate on a franchise business
-        model, with numerous branches across various countries around the world.
+        Among a plethora of reasons, Food is one of the main staples that made Canggu one of the largest digital nomad destinations in the world. From cheap traditional indonesion food and no-nonsense eateries to high-end western cuisine and astonishing Cafés, you will have no trouble finding the dish you are craving. Or let me revise that: If you patiently search through the seemingly endless amount of places to go to, you will avoid those that are just shiny on the outside and actually find the best Cafes and Restaurants in Canggu. To make your life easier, Chunks of Canggu lists all the places that I have visited and ranks them for the quality of their food and coffee as well as their ambience. This way, you are just a quick search removed to find the perfect Cafe or Restaurant that excels at just the thing you are craving.  
       </p>
     </div>
   );
